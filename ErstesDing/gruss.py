2177272bac2855749fa64df44af584ef6e964606
Gottcollo <gottcollo@gmail.com>
2025-05-22 14:36:37 +0200

    Video 4

--- ErstesDing/gruss.py
+++ ErstesDing/gruss.py
@@ -1,7 +1,16 @@
 print ("Hello Worlds")
-x=2+3
+x=2+8
 print (x) #kommentieren mit raute und nicht mit /
-print (x+7)
+print (x+7) #hier wird mit ganzzahlen und gleitzzahlen gearbeitet
 print (7.4+1.1)
 print (2.4/2*2)
 print (2.2* x )
+print ('''ich versuche was dummes
+       mit nem großen eingriff ''')
+print ("Hello " + "World!") #Hier einmal dreifach anführung gesetzt und einmal ein Print zusammengerechnet
+print ('jetzt rechnen wir mit unserem alten X und machen es neu mit einem anderen festen wert') #sagen was abgeht
+print (x)
+y = x - 3
+print (y)
+x = x + y #Hier neuen wert für X gesetzt. 
+print (x)
